9178cdb410718f2d83cac187fafc26bf741a02df
Johnny <john.r.r.allen@gmail.com>
2019-10-08 16:47:26 -0500

    Adding gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('default',function() {
+    console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function() {
+    console.log("Imagine something useful being done here.");
+});
